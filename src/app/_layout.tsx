125fa9b619e6ff2538072b0326d59e322a40cc99
Eder Andrew <ederandrew0028@gmail.com>
2025-10-28 15:37:35 -0300

    Update layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,25 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [loaded, error] = useFonts({
+        "BagelFatOne": require("../../assets/fonts/BagelFatOne-Regular.ttf"),
+    })
+
+    useEffect(()=>{
+        if (loaded || error){
+            SplashScreen.hideAsync();
+        }
+    },[loaded, error])
+
+    if (!loaded && !error){
+        return null;
+    }
+
     return (
         <Stack screenOptions={{ headerShown: false}}>
             <Stack.Screen name="(tabs)"/>
